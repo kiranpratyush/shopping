72e42308f26bdd49e1a02d62d83fe3b6cc3e4499
pratyush Kiran Rout <pratyushkiranrout@gmail.com>
2021-11-11 17:51:51 +0530

    Feat:updated State according to changes in database

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
-import Homepage from './pages/HomePage/homepage';
-import './App.css';
-import ShopPage from './pages/ShopPage/ShopPage';
-import {Route,Switch} from "react-router-dom"
-import Header from './comonents/HeaderComponent/header';
-import SignIn from './pages/SignInPage/SignIn';
-import React from 'react';
-import {authValue,addProfileData} from "./FireBase/firebase"
-
+import Homepage from "./pages/HomePage/homepage";
+import "./App.css";
+import ShopPage from "./pages/ShopPage/ShopPage";
+import { Route, Switch } from "react-router-dom";
+import Header from "./comonents/HeaderComponent/header";
+import SignIn from "./pages/SignInPage/SignIn";
+import React from "react";
+import { authValue, addProfileData } from "./FireBase/firebase";
+import { getDoc } from "firebase/firestore";
 
 class App extends React.Component {
-
-  constructor()
-  {
-    super()
-    this.state ={currentUser:null}
+  constructor() {
+    super();
+    this.state = { currentUser: null };
   }
   unsuscribe = null;
-  componentDidMount()
-  {
-    this.unsuscribe =authValue.onAuthStateChanged((user)=>
-    {
+  componentDidMount() {
+    this.unsuscribe = authValue.onAuthStateChanged((user) => {
       // this will set states when the state will be changed
-      this.setState({currentUser:user})
-      addProfileData(user)
-    })
+      if (user) {
+        const ref = addProfileData(user);
+        ref
+          .then((data) => getDoc(data))
+          .then((response) => {
+            if (response.exists()) {
+              this.setState({ currentUser: { ...response.data() } });
+            }
+          });
+      }
+    });
   }
-  componentWillUnmount()
-  {
-    this.unsuscribe()
+  componentWillUnmount() {
+    this.unsuscribe();
   }
 
-  render()
-  {
-  return (
-    <div>
-      <Header authStatus ={this.state.currentUser}/>
-      <Switch>
-      
-      <Route exact path ="/" component ={Homepage} />
-      <Route  path ="/shop" component={ShopPage}/>
-      <Route path ="/signIn" component={SignIn} />
-      
-      
-      </Switch>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Header authStatus={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signIn" component={SignIn} />
+        </Switch>
+      </div>
+    );
   }
 }
 
--- src/FireBase/firebase.js
+++ src/FireBase/firebase.js
@@ -1,71 +1,57 @@
-import {initializeApp} from 'firebase/app'
-import {getAuth,GoogleAuthProvider,signInWithPopup} from 'firebase/auth'
-import {getFirestore,doc,setDoc} from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyDuARFItIFNw46qGEbPrCjy1DNTFCBYmS4",
 
-    apiKey: "AIzaSyDuARFItIFNw46qGEbPrCjy1DNTFCBYmS4",
-  
-    authDomain: "shopping-e8997.firebaseapp.com",
-  
-    projectId: "shopping-e8997",
-  
-    storageBucket: "shopping-e8997.appspot.com",
-  
-    messagingSenderId: "551612583317",
-  
-    appId: "1:551612583317:web:f43a12e4bcea434eb0fa9c",
-  
-    measurementId: "G-8E6139F1NJ"
-  
-  };
-const app = initializeApp(firebaseConfig)
-  
+  authDomain: "shopping-e8997.firebaseapp.com",
 
-const db = getFirestore()
-const auth = getAuth(app)
+  projectId: "shopping-e8997",
+
+  storageBucket: "shopping-e8997.appspot.com",
+
+  messagingSenderId: "551612583317",
+
+  appId: "1:551612583317:web:f43a12e4bcea434eb0fa9c",
+
+  measurementId: "G-8E6139F1NJ",
+};
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-function SignIn()
-{
-    signInWithPopup(auth,provider)
-    .then((result)=>
-    {
-        const credential = GoogleAuthProvider.credentialFromResult(result)
-    })
-    .catch((error)=>
-    {
-        console.log(error)
-    })
+function SignIn() {
+  signInWithPopup(auth, provider)
+    .then((result) => {})
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
+export async function addProfileData(user) {
+  try {
+    if (!user) {
+      return;
+    } else {
+        const ref = doc(db, "users",user.uid);
+        
+      // Here I get a reference to the user document
+      const dateCreated = new Date();
+      const docData = { name: user.displayName, date: dateCreated };
 
-export async function addProfileData(user)
-{
-    try{if(!user)
-    {
-        return
+      await setDoc(ref, docData, { merge: true });
+      // set Doc merges the value if new Something is added;
+      // refer fireBase doc
+      
+      return ref
     }
-    else{
-        const ref  = doc(db,`users/${user.uid}`)
-        // Here I get a reference to the user document
-        const dateCreated = new Date()
-        const docData = {name:user.displayName,date:dateCreated}
-
-        await setDoc(ref,docData,{merge:true})
-        // set Doc merges the value if new Something is added;
-        // refer fireBase doc
-
-
-    }
-}
-    catch(e)
-    {
-        console.log(e.message)
-    }
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
-
-
-export default SignIn
-export  const authValue =auth;
+export default SignIn;
+export const authValue = auth;
